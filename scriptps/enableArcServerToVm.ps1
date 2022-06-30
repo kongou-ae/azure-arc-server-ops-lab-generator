24c9f8683680180f00f8e64f9f5112dedf511490
kongou-ae <y.matsumoto.ae@gmail.com>
2022-06-30 19:12:33 +0900

    update

--- scriptps/enableArcServerToVm.ps1
+++ scriptps/enableArcServerToVm.ps1
@@ -11,12 +11,14 @@ Start-Transcript -Path 'c:\arcsvlab-eval\enableArcServerToVm.log'
 
 Set-Item WSMan:\localhost\Client\TrustedHosts -Value "10.0.0.100"  -Force
 
-$securestring = ConvertTo-SecureString -AsPlainText $LocalAdministratorPassword -Force
-$Cred = New-Object System.Management.Automation.PSCredential("administrator",$securestring)
-$pssession = New-PSSession 10.0.0.100 -Credential $cred
+$result = Get-VM -Name arcWin2019sv01 -ErrorAction Ignore
+if ( $null -ne $result){
+    $securestring = ConvertTo-SecureString -AsPlainText $LocalAdministratorPassword -Force
+    $Cred = New-Object System.Management.Automation.PSCredential("administrator",$securestring)
+    $pssession = New-PSSession 10.0.0.100 -Credential $cred
 
-Install-module Az.ConnectedMachine -Force
-Login-AzAccount -Identity
-Connect-AzConnectedMachine -ResourceGroupName $resourceGroup -Name $name -Location $location -PSSession $pssession
-
+    Install-module Az.ConnectedMachine -Force
+    Login-AzAccount -Identity
+    Connect-AzConnectedMachine -ResourceGroupName $resourceGroup -Name $name -Location $location -PSSession $pssession
+}
 Stop-Transcript
